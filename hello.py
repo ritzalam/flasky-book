9dd632364c67c938bfdd8599beece24e7daf3c64
Richard Alam <ritzalam@gmail.com>
2017-04-16 13:16:36 -0700

    - add database

--- hello.py
+++ hello.py
@@ -6,29 +6,63 @@ from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
+from flask_sqlalchemy import SQLAlchemy
+from flask_script import Shell
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+manager = Manager(app)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+db = SQLAlchemy(app)
 
 class NameForm(FlaskForm):
 	name = StringField('What is your name?', validators=[Required()])
 	submit = SubmitField('Submit')
 
+class Role(db.Model):
+	__tablename__ = 'roles'
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(64), unique= True)
+	users = db.relationship('User', backref='role', lazy='dynamic')
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'hard to guess string'
+	def __repr__(self):
+		return '<Role %r>' % self.name
 
-manager = Manager(app)
-bootstrap = Bootstrap(app)
-moment = Moment(app)
+class User(db.Model):
+	__tablename__ = 'users'
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(64), unique=True, index=True)
+	role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+	def __repr__(self):
+		return '<User %r>' % self.username
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
 	form = NameForm()
 	if form.validate_on_submit():
-		old_name = session.get('name')
-		if old_name is not None and old_name != form.name.data:
-			flash('Looks like you have changed your name!')
+		user = User.query.filter_by(username=form.name.data).first()
+		if user is None:
+			user = User(username=form.name.data)
+			db.session.add(user)
+			session['known'] = False
+		else:
+			session['known'] = True
+
 		session['name'] = form.name.data
+		form.name.data = ''
 		return redirect(url_for('index'))
 	return render_template('index.html', form=form, name=session.get('name'),
+		known=session.get('known', False),
 		current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
@@ -42,6 +76,11 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
 	return render_template('500.html'), 500
+
+def make_shell_context():
+	return dict(app=app, db=db, User=User, Role=Role)
+
+manager.add_command("shell", Shell(make_context=make_shell_context))
 
 if __name__ == '__main__':
 	manager.run()
